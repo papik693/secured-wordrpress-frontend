4c08df7ddba025da55e160425c60218685890093
Oskar Fultowicz <oskarfultowicz@gmail.com>
2026-02-08 17:14:42 +0100

    add show/hide button to password

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,6 @@
-import type { InputHTMLAttributes } from 'react'
+import { useState, type InputHTMLAttributes } from 'react'
 import { useFormContext } from 'react-hook-form'
+import { FaEye, FaEyeSlash } from 'react-icons/fa'
 import { cn } from '../utils/cn'
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
@@ -15,29 +16,45 @@ const Input = ({
   className,
   ...props
 }: Props) => {
+  const [showPassword, setShowPassword] = useState(false)
   const {
     register,
     formState: { errors },
   } = useFormContext()
 
   const error = errors[name]?.message as string | undefined
+  const inputType =
+    type === 'password' ? (showPassword ? 'text' : 'password') : type
 
   return (
     <div className={cn('flex flex-col gap-2', className)}>
       <label htmlFor={name} className="text-lg">
         {label}
       </label>
-      <input
-        id={name}
-        type={type}
-        placeholder={placeholder}
-        {...register(name)}
-        className={cn(
-          'rounded-lg p-2 text-lg bg-[#0a0a0a] border border-[#292929]',
-          error && 'border-red-500',
+      <div className="relative w-full">
+        <input
+          id={name}
+          type={inputType}
+          placeholder={placeholder}
+          {...register(name)}
+          className={cn(
+            'rounded-lg p-2 text-lg bg-[#0a0a0a] border border-[#292929] w-full',
+            error && 'border-red-500',
+            type === 'password' && 'pr-10',
+          )}
+          {...props}
+        />
+        {type === 'password' && (
+          <button
+            className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
+            type="button"
+            onClick={() => setShowPassword((prev) => !prev)}
+          >
+            {inputType === 'password' ? <FaEye /> : <FaEyeSlash />}
+          </button>
         )}
-        {...props}
-      />
+      </div>
+
       {error && <span className="text-red-500 text-sm">{error}</span>}
     </div>
   )
