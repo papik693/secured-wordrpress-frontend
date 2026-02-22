b89c80d6486b1ed7751e0fe579b683013104534d
Oskar Fultowicz <oskarfultowicz@gmail.com>
2026-02-22 17:04:10 +0100

    make table user menu close after choosing action

--- src/components/UserTableMenu.tsx
+++ src/components/UserTableMenu.tsx
@@ -6,7 +6,6 @@ import Confirm from './Confirm'
 type Props = {
   id: number
   is_staff: boolean
-
   closeMenu: () => void
 }
 
@@ -26,25 +25,30 @@ const UserTableMenu = ({ id, is_staff, closeMenu }: Props) => {
       deleteUser(id)
     }
     setAction(null)
+    closeMenu()
   }
 
   return (
     <>
-      <div className="fixed inset-0 z-10" onClick={closeMenu} />
-      <div className="fixed bottom-0 w-full md:absolute md:bottom-auto right-0 mt-2 md:w-56 rounded-xl bg-[#111] border border-white/10 shadow-2xl py-2 z-20 overflow-hidden px-1 transition">
-        <button
-          onClick={() => setAction('toggle')}
-          className="flex items-center px-3 py-2 text-sm  hover:bg-white/5 hover:text-green-400 rounded-lg transition-colors  w-full  text-left cursor-pointer text-green-300"
-        >
-          {is_staff ? 'Remove Permission' : 'Allow User'}
-        </button>
-        <button
-          onClick={() => setAction('delete')}
-          className="flex items-center px-3 py-2 text-sm  hover:bg-white/5 hover:text-red-500 rounded-lg transition-colors  w-full  text-left  cursor-pointer text-red-400"
-        >
-          Delete User
-        </button>
-      </div>
+      {action === null && (
+        <>
+          <div className="fixed inset-0 z-10" onClick={closeMenu} />
+          <div className="fixed bottom-0 w-full md:absolute md:bottom-auto right-0 mt-2 md:w-56 rounded-xl bg-[#111] border border-white/10 shadow-2xl py-2 z-20 overflow-hidden px-1 transition">
+            <button
+              onClick={() => setAction('toggle')}
+              className="flex items-center px-3 py-2 text-sm  hover:bg-white/5 hover:text-green-400 rounded-lg transition-colors  w-full  text-left cursor-pointer text-green-300"
+            >
+              {is_staff ? 'Remove Permission' : 'Allow User'}
+            </button>
+            <button
+              onClick={() => setAction('delete')}
+              className="flex items-center px-3 py-2 text-sm  hover:bg-white/5 hover:text-red-500 rounded-lg transition-colors  w-full  text-left  cursor-pointer text-red-400"
+            >
+              Delete User
+            </button>
+          </div>
+        </>
+      )}
       <Confirm
         open={action !== null}
         onCancel={() => setAction(null)}
