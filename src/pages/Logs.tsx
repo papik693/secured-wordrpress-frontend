1a73d6a0163a9c701d565d7154a6f5842a18612f
Oskar Fultowicz <oskarfultowicz@gmail.com>
2026-03-24 20:03:28 +0100

    create Logs page with static data

--- src/pages/Logs.tsx
+++ src/pages/Logs.tsx
@@ -1,9 +1,28 @@
+import clsx from 'clsx'
 import PageTitle from '../components/PageTitle'
+import { useGetLogs } from '../hooks/useGetLogs'
 
 const Logs = () => {
+  const { logs } = useGetLogs()
   return (
     <div className="p-6">
       <PageTitle>Logs</PageTitle>
+      <div className="border border-[#FFFFFF16] h-100 rounded-lg font-mono p-4 flex flex-col justify-end">
+        {logs.map((log) => {
+          const formatLog = `[${log['@timestamp']}] ${log.method} ${log.clientIp} ${log.country} ${log.action} ${log.terminatingRuleId} ${log.terminatingRuleType}`
+          return (
+            <p
+              key={log['@timestamp']}
+              className={clsx(
+                log.action === 'BLOCK' && 'text-red-500',
+                'text-sm',
+              )}
+            >
+              {formatLog}
+            </p>
+          )
+        })}
+      </div>
     </div>
   )
 }
