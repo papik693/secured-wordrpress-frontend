ffb90538a3bac8870014acf98ce23d1810a66f19
Oskar Fultowicz <oskarfultowicz@gmail.com>
2026-03-24 20:09:18 +0100

    change wrong feature for tailwind cn

--- src/pages/Logs.tsx
+++ src/pages/Logs.tsx
@@ -1,6 +1,6 @@
-import clsx from 'clsx'
 import PageTitle from '../components/PageTitle'
 import { useGetLogs } from '../hooks/useGetLogs'
+import { cn } from '../utils/cn'
 
 const Logs = () => {
   const { logs } = useGetLogs()
@@ -9,12 +9,12 @@ const Logs = () => {
       <PageTitle>Logs</PageTitle>
       <div className="border border-[#FFFFFF16] h-100 rounded-lg font-mono p-4 flex flex-col justify-end">
         {logs.map((log) => {
-          const formatLog = `[${log['@timestamp']}] ${log.method} ${log.clientIp} ${log.country} ${log.action} ${log.terminatingRuleId} ${log.terminatingRuleType}`
+          const formatLog = `[${log['@timestamp']}] ${log.action} ${log.method} ${log.clientIp} ${log.country}  ${log.terminatingRuleId} ${log.terminatingRuleType}`
           return (
             <p
               key={log['@timestamp']}
-              className={clsx(
-                log.action === 'BLOCK' && 'text-red-500',
+              className={cn(
+                log.action === 'BLOCK' && 'text-red-700',
                 'text-sm',
               )}
             >
